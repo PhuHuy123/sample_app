fa5d886cd6d0fc44851bd4b51006985e6b1706a2
PhuHuy123 <nguyenphuhuy180920@gmail.com>
2022-03-30 10:06:35 +0700

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ get 'help'=> 'static_pages#help'
 get 'about'=> 'static_pages#about'
 get 'contact' => 'static_pages#contact'
 get 'signup' => 'users#new'
+resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
