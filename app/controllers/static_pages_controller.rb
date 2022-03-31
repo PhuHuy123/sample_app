53a19b61923406fb39747461bab817a520c28cf6
PhuHuy123 <nguyenphuhuy180920@gmail.com>
2022-03-31 14:11:02 +0700

    Add user following

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,9 +1,7 @@
 class StaticPagesController < ApplicationController
-  # @micropost = current_user.microposts.build if logged_in?
 
-  # @feed_items = current_user.feed.paginate(page: params[:page]) if logged_in?
   def home
-    # @micropost = current_user.microposts.build if logged_in?
+    
     if logged_in?
       
       # binding.break  
--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,22 @@
 Rails.application.routes.draw do
-  get 'password_resets/new'
-  get 'password_resets/edit'
-  # get 'users/new'
-  root'static_pages#home'
-get 'help'=> 'static_pages#help'
-get 'about'=> 'static_pages#about'
+
+root 'static_pages#home'
+get 'help' => 'static_pages#help'
+get 'about' => 'static_pages#about'
 get 'contact' => 'static_pages#contact'
 get 'signup' => 'users#new'
 get 'login' => 'sessions#new'
 post 'login' => 'sessions#create'
 delete 'logout' => 'sessions#destroy'
-resources :users
-resources :account_activations, only: [:edit]
-resources :password_resets,only: [:new, :create, :edit, :update]
-resources :microposts,only: [:create, :destroy]
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+resources :users do
+  member do
+  get :following, :followers
+  end
+end
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+resources :account_activations,   only: [:edit]
+resources :password_resets,       only: [:new, :create, :edit, :update]
+resources :microposts,            only: [:create, :destroy]
+resources :relationships,         only: [:create, :destroy]
+
 end
